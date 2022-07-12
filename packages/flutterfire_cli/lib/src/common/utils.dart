e295c4104cedda86a8a3a7c57fa065524f0c390f
Salah Nahed <salahelamassi@gmail.com>
2022-07-13 02:48:14 +0300

    add GoogleService Info.plist to Runner target

--- packages/flutterfire_cli/lib/src/common/utils.dart
+++ packages/flutterfire_cli/lib/src/common/utils.dart
@@ -234,7 +234,9 @@ end
 
 # Write only if config doesn't exist
 if googleConfigExists == false
-  project.new_file(googleFile)
+    file = project.new_file(googleFile)
+    main_target = project.targets.first
+    main_target.add_file_references([file])
   project.save
 end
 ''';
