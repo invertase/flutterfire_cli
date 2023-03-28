4ca0cf7b540ad9193c7e037e21e62e376ccf49b1
russellwheatley <russellwheatley85@gmail.com>
2023-03-28 10:40:54 +0100

    reorder imports

--- packages/flutterfire_cli/lib/src/command_runner.dart
+++ packages/flutterfire_cli/lib/src/command_runner.dart
@@ -18,6 +18,7 @@
 import 'package:args/command_runner.dart';
 
 import 'commands/bundle_service_file.dart';
+import 'commands/config-update.dart';
 import 'commands/config.dart';
 import 'commands/update.dart';
 import 'commands/upload_symbols.dart';
@@ -56,5 +57,6 @@ class FlutterFireCommandRunner extends CommandRunner<void> {
     addCommand(UpdateCommand(flutterApp));
     addCommand(UploadCrashlyticsSymbols(flutterApp));
     addCommand(BundleServiceFile(flutterApp));
+    addCommand(ConfigUpdate(flutterApp));
   }
 }
