f212c7213cca91aa734b23e085ec36d054d53e84
russellwheatley <russellwheatley85@gmail.com>
2023-04-26 12:03:28 +0100

    feat: overwrite apple service file

--- packages/flutterfire_cli/lib/src/common/strings.dart
+++ packages/flutterfire_cli/lib/src/common/strings.dart
@@ -64,9 +64,6 @@ const logLearnMoreAboutCli =
     'Learn more about using this file and next steps from the documentation:\n'
     ' > $firebaseDocumentationUrl';
 
-const serviceFileAlreadyExists =
-    'Your $appleServiceFileName already exists, skipping write... ';
-
 /// Prompts when Android Google Services JSON file already exists but contains
 /// configuration values for a different Firebase project.
 String logPromptReplaceGoogleServicesJson(
