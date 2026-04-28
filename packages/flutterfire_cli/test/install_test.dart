2f7e83eceef001f2926224bb438751b43aa6f0dd
Ernesto Ramirez <neartox@outlook.com>
2026-04-28 16:35:08 -0600

    add test

--- packages/flutterfire_cli/test/install_test.dart
+++ packages/flutterfire_cli/test/install_test.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:async/async.dart';
 import 'package:path/path.dart' as p;
 import 'package:pubspec_parse/pubspec_parse.dart';
 import 'package:test/test.dart';
 
 import 'test_utils.dart';
+
+Future<bool> _waitForLine(StreamQueue<String> queue, String content) async {
+  while (await queue.hasNext) {
+    final line = await queue.next;
+    if (line.contains(content)) return true;
+  }
+  return false;
+}
 
 void main() {
   String? projectPath;
@@ -131,7 +141,6 @@ void main() {
       Duration(minutes: 2),
     ),
   );
-
   test(
     'Installs then use --only-pubspec-plugins flag to remove dependency',
     () async {
@@ -184,6 +193,90 @@ void main() {
       expect(parsedPubspec2.dependencies['cloud_firestore'], isNull);
 
       expect(result.stdout.toString().contains('Successfully installed'), true);
+    },
+    timeout: const Timeout(
+      Duration(minutes: 2),
+    ),
+  );
+  test(
+    'Installs from CLI',
+    () async {
+      final process = await Process.start(
+        'flutterfire',
+        [
+          'install',
+          '4.10.0',
+        ],
+        workingDirectory: projectPath,
+        runInShell: true,
+      );
+      final stdoutLines = StreamQueue(
+        process.stdout
+            .transform(utf8.decoder) /*.transform(const LineSplitter())*/,
+      );
+
+      await _waitForLine(
+        stdoutLines,
+        'Select the Firebase plugins you would like to install',
+      );
+
+      // Core
+      process.stdin.write(' ');
+
+      // Analytics
+      process.stdin.write('\x1b[B');
+      process.stdin.write(' ');
+
+      // Crashlytics
+      process.stdin.write('\x1b[B' * 4);
+      process.stdin.write(' ');
+
+      // Messaging
+      process.stdin.write('\x1b[B' * 5);
+      process.stdin.write(' ');
+
+      // Performance
+      process.stdin.write('\x1b[B' * 2);
+      process.stdin.write(' ');
+
+      // Remote Config
+      process.stdin.write('\x1b[B');
+      process.stdin.write(' ');
+
+      // confirm selection
+      process.stdin.write('\r');
+
+      await process.stdin.flush();
+
+      final installSuccess =
+          await _waitForLine(stdoutLines, 'Successfully installed');
+      expect(installSuccess, isTrue);
+
+      // finish
+      await process.stdin.flush();
+      await process.stdin.close();
+
+      if (await process.exitCode != 0) {
+        final errorOutput = await process.stderr.transform(utf8.decoder).join();
+        fail(errorOutput);
+      }
+
+      final parsedPubspec = Pubspec.parse(
+        await File(p.join(projectPath!, 'pubspec.yaml')).readAsString(),
+      );
+
+      expect(parsedPubspec.dependencies['firebase_analytics'], isNotNull);
+      expect(parsedPubspec.dependencies['firebase_core'], isNotNull);
+      expect(parsedPubspec.dependencies['firebase_performance'], isNotNull);
+      expect(parsedPubspec.dependencies['firebase_remote_config'], isNotNull);
+      expect(parsedPubspec.dependencies['firebase_crashlytics'], isNotNull);
+      expect(parsedPubspec.dependencies['firebase_messaging'], isNotNull);
+
+      expect(parsedPubspec.dependencies['firebase_in_app_messaging'], isNull);
+      expect(
+        parsedPubspec.dependencies['firebase_ml_model_downloader'],
+        isNull,
+      );
     },
     timeout: const Timeout(
       Duration(minutes: 2),
